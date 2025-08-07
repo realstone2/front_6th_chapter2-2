f0a9c2043ef7fac1aa2322badf7ecd23c61a051c
진돌 <wlstjrghdud@naver.com>
2025-08-08 02:13:40 +0900

    feat: Vite 설정 파일 업데이트 및 GitHub Pages 배포 워크플로우 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,46 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    root: ".",
+    base: "/front_6th_chapter2-2/",
+    build: {
+      rollupOptions: {
+        input: "./index.advanced.html",
+      },
+      outDir: "dist",
+      copyPublicDir: true,
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
   })
-)
+);
+
+// import { defineConfig } from 'vitest/config';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   root: '.',
+//   base: '/front_6th_chapter2-1/',
+//   build: {
+//     rollupOptions: {
+//       input: './index.html',
+//     },
+//     outDir: 'dist',
+//     copyPublicDir: true,
+//   },
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: 'src/setupTests.js',
+//   },
+// });
